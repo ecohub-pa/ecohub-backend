42e3031780c785986a75337f90038054a28fa5ca
João Gabriel <joaogabrielbochlivio@gmail.com>
2024-05-13 21:17:06 -0300

    adiciona rota para listar projetos

---
+++ src/routes/projects/list-projects.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { prisma } from '../../lib/prisma'
+
+export async function listProjects(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get('/projects', {}, async (request, reply) => {
+    const projects = await prisma.project.findMany({
+      include: {
+        user: true,
+      },
+    })
+
+    return reply.status(200).send({ projects })
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { createAccount } from './routes/auth/create-account'
 import { createProject } from './routes/projects/create-project'
 import { errorHandler } from './error-handler'
+import { listProjects } from './routes/projects/list-projects'
 
 const app = fastify()
 
@@ -13,6 +14,7 @@ app.setErrorHandler(errorHandler)
 
 app.register(createAccount)
 app.register(createProject)
+app.register(listProjects)
 
 app.listen({ port: 3000 }).then(() => {
   console.log('Server running...')
